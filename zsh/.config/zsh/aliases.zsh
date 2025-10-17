94ada151efa52af2bca89cf223717f55dacd70fd
maxdcmn <maxdecman@me.com>
2025-10-17 17:51:47 +0200

    add new aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -7,5 +7,9 @@ mkcd () {
 alias v='nvim'
 alias r='ranger'
 alias g='lazygit'
+alias d='docker'
+alias k='kubectl'
 alias go='cd $(fzf --walker=dir,follow,hidden --walker-skip=Library,Applications,.local,.cache,.git,miniconda3,node_modules,.vscode,.cargo)'
 alias c='cd'
+fh() { history 1 | fzf | sed "s/ *[0-9]* *//" | pbcopy; }
+roll() { echo $(( (RANDOM % (${1:-6})) + 1 )); }
