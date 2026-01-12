adea0691071f64a084b845db959bbc0eb78fd593
maxdcmn <maxdecman@me.com>
2026-01-12 13:49:43 +0100

    update keymaps and aliases

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace word under cursor" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result and center view" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result and center view" })
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent selected lines" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent selected lines" })
 
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and maintain cursor position" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias r='ranger'
 alias g='lazygit'
 alias d='docker'
 alias k='kubectl'
-alias go='cd $(fzf --walker=dir,follow,hidden --walker-skip=Library,Applications,.local,.cache,.git,miniconda3,node_modules,.vscode,.cargo)'
+alias f='cd $(fzf --walker=dir,follow,hidden --walker-skip=Library,Applications,.local,.cache,.git,miniconda3,node_modules,.vscode,.cargo)'
 alias c='cd'
 fh() { history 1 | fzf | sed "s/ *[0-9]* *//" | pbcopy; }
 roll() { echo $(( (RANDOM % (${1:-6})) + 1 )); }
