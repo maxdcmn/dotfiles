f940f7f05cdea52b11d5cd24b10f5fc5846887f4
maxdcmn <maxdecman@me.com>
2026-02-25 17:16:30 +0100

    add aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -11,6 +11,8 @@ alias d='docker'
 alias k='kubectl'
 alias f='cd $(fzf --walker=dir,follow,hidden --walker-skip=Library,Applications,.local,.cache,.git,miniconda3,node_modules,.vscode,.cargo)'
 alias ff='fastfetch'
+alias ports='lsof -i -P -n | grep LISTEN'
+alias gl='git log --graph --pretty=format:"%C(yellow)%h%C(reset) %C(cyan)%ar%C(reset) %s %C(dim)— %an%C(reset)%C(auto)%d" --abbrev-commit -20'
 fh() { history 1 | fzf | sed "s/ *[0-9]* *//" | $CLIP; }
 roll() { echo $(( (RANDOM % (${1:-6})) + 1 )); }
 
